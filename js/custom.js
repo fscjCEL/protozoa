48a7dabf7740b83120806c8f7f054e95ee7ea846
ianvanhoof <ian@ianvanhoof.com>
2016-09-22 15:05:33 -0400

    Added helper classes, and filled out the example demo file.

--- js/custom.js
+++ js/custom.js
@@ -67,3 +67,70 @@ var accordionHeaders = document.getElementsByClassName("cel-accordionHeader");
 for(var ah=0;ah<accordionHeaders.length;ah++){
     accordionHeaders[ah].addEventListener("click",accordionMethod.celAccordionHandler,"false");
 }//end for
+
+var theTabularContainers = document.getElementsByClassName("cel-tabular-container");
+for(var z=0;z<theTabularContainers.length;z++){
+    theTabularContainers[z].classList.add("is-card");
+    //looping through each tabular group
+    //grab the all the nav elements within that container.
+    var theNavsWithin =  theTabularContainers[z].getElementsByClassName("cel-tabNav");
+    var theBase = 100 - theNavsWithin.length;
+    var tabNavWidthPercentage = theBase/theNavsWithin.length;
+    for(var a=0;a<theNavsWithin.length;a++){
+        theNavsWithin[a].style.width = tabNavWidthPercentage+"%";
+    }//end for
+}//end for
+var tabularMethod = {
+    tabularHandler:function(){
+        //this is called when a tab is clicked and changes the content to the tab clicked upon..
+        var theClickedTabNav = this;
+
+        var theTabularParent = this.parentNode.parentNode;
+        var theNumberOfClickedTabNav = theClickedTabNav.id.substr(-1);
+        var theIDOfNeededTab = theTabularParent.id+"-tab-"+theNumberOfClickedTabNav;
+        //remove the active classname off the currently active tabNav
+        theTabularParent.getElementsByClassName("is-activeTabNav")[0].classList.remove("is-activeTabNav");
+        //hide the currently active tab
+        theTabularParent.getElementsByClassName("is-activeTab")[0].classList.add("is-invisible");
+        //remove the active designation from currently active tab
+        theTabularParent.getElementsByClassName("is-activeTab")[0].classList.remove("is-activeTab");
+
+        //Add the active class to the tabNav you just clicked.
+        theClickedTabNav.classList.add("is-activeTabNav");
+        //show the newly clicked tab content
+        document.getElementById(theIDOfNeededTab).classList.remove("is-invisible");
+        document.getElementById(theIDOfNeededTab).classList.add("is-activeTab");
+    }//end tabularfunction
+}; //end method
+
+var theTabNavs = document.getElementsByClassName("cel-tabNav");
+var theTabs = document.getElementsByClassName("cel-tab");
+for (var x=0;x < theTabNavs.length;x++){
+    theTabNavs[x].addEventListener("click",tabularMethod.tabularHandler,false);
+    var theTabNavParentID = theTabNavs[x].parentNode.parentNode.id;
+    theTabNavs[x].id = theTabNavParentID+"-nav-"+x;
+}//end for
+for(var y=0;y<theTabs.length;y++){
+    var theTabParentID = theTabs[y].parentNode.parentNode.id;
+    theTabs[y].id = theTabParentID+"-tab-"+y;
+}//end for
+var clickRevealMethod = {
+    revealHandler:function(){
+        var getThisElement,theElementIWantTOReveal;
+        getThisElement = this.getAttribute("data-reveal-ID");
+        //grab that matching element
+        theElementIWantTOReveal = document.getElementById(getThisElement);
+        //if it's visible (show), if not then (hide).
+        if(theElementIWantTOReveal.className === "theReveal is-invisible"){
+            theElementIWantTOReveal.className = "theReveal";
+        }else{
+            theElementIWantTOReveal.className = "theReveal is-invisible";
+        }
+        //end if
+    }//end function
+};//end click reveal method
+
+var clickRevealHeaders = document.getElementsByClassName("clickButton");
+for(var yz=0;yz<clickRevealHeaders.length;yz++){
+    clickRevealHeaders[yz].addEventListener("click",clickRevealMethod.revealHandler,"false");
+}//end for
